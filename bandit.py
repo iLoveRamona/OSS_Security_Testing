9f17647afb742266983e8b69243df64657c3519d
sidea <ssa25t@mail.ru>
2025-07-03 21:24:38 +0300

    delete trash file

--- bandit.py
+++
@@ -1,5 +0,0 @@
-import bandit
-d = bandit.get_distribution(__package__)
-metadata = d._get_metadata(d.PKG_INFO)
-home_page = [m for m in metadata if m.startswith('Home-page:')]
-url = home_page[0].split(':', 1)[1].strip()
