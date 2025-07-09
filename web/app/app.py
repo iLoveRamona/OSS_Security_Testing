e4e4cf81c8525c8c1145e2a1a454366cb714e891
nikita <beryuhovnikita@yandex.ru>
2025-07-09 15:42:53 +0300

    add API

--- web/app/app.py
+++ web/app/app.py
@@ -1,22 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-import asyncio
-from contextlib import asynccontextmanager
+import bcrypt
 
-from rabbit import add_request, get_request
-from db import get_report
+from rabbit import add_request
+from db import get_report, edit_report
 import verification
 from config import config
 
 app = FastAPI()
 
-# pkg:pypi/requests@2.31.0
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    asyncio.create_task(get_request())
-    yield
+users = {"rabbit"}
 
-    
+# pkg:pypi/requests@2.31.0
+
+@app.post("/api/{manager}:{index}/{name}@{version}")
+def add_report(manager: str, index: str, name: str, version: str, data = Body()):
+    purl = f"{manager}:{index}/{name}@{version}"
+    passwd = data["passwd"]
+    user = data["user"]
+    if user and user in users and passwd and not bcrypt.checkpw(passwd.encode(), config["heshPass"]):
+        return "lol" 
+    edit_report(purl, data["report"])
+    return "ok"
+
+
 @app.get("/")
 def read_root():
     html_content = "<h2>Hello OSS security testing!</h2>"
--- web/app/rabbit.py
+++ web/app/rabbit.py
@@ -21,23 +21,3 @@ def add_request(purl):
     channel.basic_publish(exchange='airflow', routing_key='start', body=json.dumps(body))
 
     connection.close()
-
-async def get_request():
-    credentials = pika.PlainCredentials(username, password)
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=ip_server[:ip_server.index(':')],
-            credentials=credentials))
-    channel = connection.channel()
-
-    def callback(ch, method, properties, body):
-        data_dict = json.loads(body) 
-        edit_report(data_dict["purl"], data_dict["report"])
-        return body
-
-    channel.basic_consume(queue='airflow.end', on_message_callback=callback, auto_ack=True)
-
-
-    channel.start_consuming()
-
-    connection.close()
