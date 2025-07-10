b3f214331332ba27b36ff1c98f13eea271beab12
m4xxx1m <mi4469228@yandex.ru>
2025-07-10 10:37:16 +0300

    Rebase semgrep

--- workers/semgrep/scan.py
+++ workers/semgrep/scan.py
@@ -1,10 +1,18 @@
 import subprocess
+from scanner import config
+import sys
 
 class Scanner:
     def __init__(self):
-        pass
+        print(f'semgrep token: {config.SEMGREP_APP_TOKEN}')
+        result = subprocess.run(
+            f'SEMGREP_APP_TOKEN={config.SEMGREP_APP_TOKEN} semgrep login',
+            shell=True
+        )
+        if result.returncode != 0:
+            print('SEMGREP_APP_TOKEN not provided', file=sys.stderr)
 
-    def scan_repo(target_path):
+    def scan_repo(self, target_path):
         result = subprocess.run(
             f"semgrep --config=auto --json-output=/tmp/semgrep_result.json {target_path}",
             shell=True
